f27aa6919ef2260c4b92d989f62580e93d9b2206
Justin Alcendor <jcabig24@outlook.com>
2026-08-13 12:47:56 -0500

    Add: valid-word-square - submission-0

---
+++ Data Structures & Algorithms/valid-word-square/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        // Iterate through words in the array
+        for (int i = 0; i < words.size(); ++i){
+            for (int j = 0; j < words.get(i).length(); ++j){
+                // Check if the character of the row and characters of the col equals each other or if the characters of i doesn't equal the characters of j
+                if (j >= words.size() || i >= words.get(j).length() || words.get(j).charAt(i) != words.get(i).charAt(j)) return false;    
+            }      
+    }
+    // return true
+            return true;
+}
+}
