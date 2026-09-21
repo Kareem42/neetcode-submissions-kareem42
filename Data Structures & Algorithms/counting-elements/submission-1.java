89f7ddcf258388a4f195540f98af10e13614d06c
Justin Alcendor <jcabig24@outlook.com>
2026-09-21 09:26:16 -0500

    Add: counting-elements - submission-1

---
+++ Data Structures & Algorithms/counting-elements/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countElements(int[] arr) {
+        Set<Integer> hashSet = new HashSet<>();
+
+        for (int i : arr){
+            hashSet.add(i);
+        }
+
+        int count = 0;
+
+        for(int i : arr){
+            if (hashSet.contains(i + 1)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
